3423f8f068fa8ef49977d19fd3394a9b042ed098
Pierrot.METILLE <Pierrot.METILLE@SC-C315-PC09>
2022-11-24 09:12:23 +0100

    Add ABC classe

---
+++ Projet 24.11.2022/A.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_24._11._2022
+{
+    internal class Class1
+    {
+    }
+}
