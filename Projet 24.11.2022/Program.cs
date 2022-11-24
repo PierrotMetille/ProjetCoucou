8f79a6c52b3b47d64268d66959e7cdf55de16102
Pierrot.METILLE <Pierrot.METILLE@SC-C315-PC09>
2022-11-24 09:06:22 +0100

    Add project files.

---
+++ Projet 24.11.2022/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Transactions;
+using System;
+using System.IO;
+
+namespace program
+{
+    
+    public class call
+    {
+        public static void Main() 
+        { 
+            string saisie;
+
+            Console.WriteLine("enter an option : ");
+            saisie = Console.ReadLine();
+
+            switch (saisie)
+            {
+                case "a":
+                    Console.WriteLine("Coucou, je suis A");
+                    break;
+                case "b":
+                    Console.WriteLine("Coucou, je suis A et B");
+                    break;
+                case "c":
+                    Console.WriteLine("Je suis malade avec un certificat`!");
+                    break;
+                default:
+                    Console.WriteLine("This is not an unvaliable option");
+                    break;
+            }
+
+        }
+
+
+    }
+
+
+
+
+}
